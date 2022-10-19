3f2d2e76f1af0bef80d167585e11811eee01453d
kasta <kostkaru@mail/ru>
2022-10-19 22:47:38 +0300

    homework 2

--- lesson_TWO/1/Program.cs
+++ lesson_TWO/1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и
+ на выходе показывает вторую цифру этого числа.
+  (Использовать только математические операции, нельзя использовать число как строку по типу number[1])
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int random = new Random().Next(100, 1000);
+int res = Method(random);
+Console.WriteLine($"{random} -> {res}");
+
+int Method(int a)
+{
+    int result = random / 10 - (random / 100) * 10;
+    return result;
+}
