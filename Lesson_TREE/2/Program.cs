57524a8f68cbf1b34e8c08b1d15bc6a6635f97b1
kasta <kostkaru@mail/ru>
2022-10-22 19:56:29 +0300

    g Please enter the commit message for your changes. Lines starting

---
+++ Lesson_TREE/2/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int Read()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+double[] masA = { 1, 2, 3 };
+double[] masB = { 1, 2, 3 };
+
+
+
+for (int j = 0; j < 3; j++)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        if (i == 0)
+        {
+            Console.WriteLine($"vvedite a{j + 1}");
+            masA[j] = Read();
+        }
+        else
+        {
+            Console.WriteLine($"vvedite b{j + 1}");
+            masB[j] = Read();
+        }
+    }
+}
+
+
+//не успеваю засунуть в цикл нахождение длинны ,оставлю грубо по формуле (метод рекурсии можно было использовать при нахождении квадрата разницы)
+
+double l = Math.Pow(Math.Pow((masA[0] - masB[0]), 2) + Math.Pow((masA[1] - masB[1]), 2) + Math.Pow((masA[2] - masB[2]), 2), 0.5);
+Console.WriteLine(l);
+
+
+
---
+++ Lesson_TREE/3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.WriteLine($"vvedite chislo");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a == 0) //проверка условия >0
+{
+    Console.WriteLine($"vvedite chislo bolshe 0");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+for (double i = 1; i < a + 1; i++)
+{
+    double step = Math.Pow(i, 3);
+    Console.Write($"{step} ");
+}
+
+
+
