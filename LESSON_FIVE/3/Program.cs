4601956f388f7b7f372c0f74327a6e5e4ce3a9ac
kasta <kostkaru@mail/ru>
2022-11-11 19:15:17 +0300

    e Please enter the commit messege for your changes. Lines starting

---
+++ LESSON_FIVE/3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+минимальным значениями элементов массива.
+
+[3 7 22 2 78] -> 76*/
+int[] mass = { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < mass.Length; i++)
+{
+
+    mass[i] = new Random().Next(1, 100); ;
+    Console.Write($" {mass[i]}");
+}
+int min = mass[0];
+int max = mass[0];
+
+
+foreach (int el in mass)
+{
+    if (el < min) min = el;
+    if (el > max) max = el;
+}
+Console.Write($" - min={min} + max={max} res={max - min}");
