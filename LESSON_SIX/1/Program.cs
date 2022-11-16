375eefef4cdf7f53b9c24a5e6fdec4e0badb436d
kasta <kostkaru@mail/ru>
2022-11-16 22:24:27 +0300

    f Please enter the commit message for your changes. Lines starting

---
+++ LESSON_SIX/1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+/*Console.WriteLine("Введите числа через пробел");
+string values = Console.ReadLine();
+
+string s10 = string.Join(" ", values);
+int strong = Convert.ToInt32(s10);
+Console.WriteLine(strong);
+*/
+Console.WriteLine("Введите числа через пробел");
+string text = Console.ReadLine();
+
+if (text != "")
+{
+    string[] words = text.Split(new char[] { ' ' });
+    int num = 0;
+    int[] mass = new int[words.Length];
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        mass[i] = Convert.ToInt32(words[i]);
+    }
+    foreach (int i in mass)
+    {
+        if (i > 0) num++;
+    }
+
+    Console.WriteLine("Положительных = " + num);
+}
+else
+{
+    Console.WriteLine("введите числа корректно");
+}
---
+++ LESSON_SIX/2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double Conv(string b)
+{
+    Console.WriteLine($"VVedite {b}");
+    string s = Console.ReadLine();
+    double i = Convert.ToDouble(s);
+    return i;
+}
+
+double[,] mass = { { Conv("b1"), Conv("k1") }, { Conv("b2"), Conv("k2") } };
+
+//(b2 - b1) / (k1 - k2)
+
+double x = (mass[1, 0] - mass[0, 0]) / (mass[0, 1] - mass[1, 1]);
+//k1 * x + b1
+double y = (mass[0, 1] * x) + mass[0, 0];
+
+Console.WriteLine($"точка пересечения M({x};{y})");
+
+
+
+
