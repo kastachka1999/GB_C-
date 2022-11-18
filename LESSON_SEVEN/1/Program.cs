a1d23d138ad940d37796e2d5a4f375b590efc570
kasta <kostkaru@mail/ru>
2022-11-18 23:09:37 +0300

    e e Please enter the commit message for your changes. Lines starting

---
+++ LESSON_SEVEN/1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+int m = Conv("количесво строк");
+int n = Conv("количество столбцов");
+
+double[,] mass = new double[m, n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = Math.Round(new Random().NextDouble(), 1) + (new Random().Next(-9, 10));
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int Conv(string b)
+{
+    Console.WriteLine($"VVedite {b}");
+    string s = Console.ReadLine();
+    int i = Convert.ToInt32(s);
+    return i;
+}
---
+++ LESSON_SEVEN/2/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет. 
+Во вводе первая цифра - номер строки, вторая - столбца.
+
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+int x = 0;
+int y = 0;
+int i = 0;
+Console.WriteLine("ВВедите строку и столбец элемента");
+string str = Console.ReadLine();
+
+if (str.Length == 2)
+{
+
+
+    foreach (char l in str)
+    {
+        if (i == 0)
+        {
+            x = Convert.ToInt32(l) - 48;
+            i++;
+        }
+        else
+        {
+            y = Convert.ToInt32(l) - 48;
+            i--;
+        }
+    }
+
+
+    int[,] mass = new int[4, 4];
+
+
+
+    if ((mass.GetLength(0) - x) >= 1 || (mass.GetLength(1) - y) >= 1)
+    {
+        Console.WriteLine(mass[x, y]);
+    }
+    else
+    {
+        Console.WriteLine("nothing this element in array");
+    }
+}
+else
+{
+    Console.WriteLine("ВВедите двухзначное число");
+}
